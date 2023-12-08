a7bf37f9bb263e3129256f75d8947ca9489d307e
Karloph <v.labusov@g.nsu.ru>
2023-12-08 09:43:46 +0700

    Update Polynomial.java

--- Task_1_1_2/src/main/java/org/task/Polynomial.java
+++ Task_1_1_2/src/main/java/org/task/Polynomial.java
@@ -6,15 +6,15 @@ public class Polynomial {
 
     public int[] cfs;
 
-    public Polynomial() { // пустой многочлен
+    public Polynomial() { //пустой многочлен
         cfs = new int[0];
     }
 
-    public Polynomial(int[] list) { // непустой многочлен
+    public Polynomial(int[] list) { //непустой многочлен
         cfs = list;
     }
 
-    public Polynomial plus(Polynomial list) { // сложение многочленов
+    public Polynomial plus(Polynomial list) { //сложение многочленов
         Polynomial tmp = new Polynomial(new int[Math.max(cfs.length, list.cfs.length)]);
         if (cfs.length == list.cfs.length){ //если равны по длине
             for (int i = 0; i < list.cfs.length; i++){
@@ -48,7 +48,7 @@ public class Polynomial {
         return tmp;
     }
 
-    public Polynomial minus(Polynomial list) { // вычитание многочленов
+    public Polynomial minus(Polynomial list) { //вычитание многочленов
         Polynomial tmp = new Polynomial(new int[Math.max(cfs.length, list.cfs.length)]);
         if (cfs.length == list.cfs.length){ // если равны
             for (int i = 0; i < list.cfs.length; i++){
@@ -57,7 +57,7 @@ public class Polynomial {
         }
         else{
             int tmpLen = 0;
-            if (cfs.length < list.cfs.length){ // если первое меньше
+            if (cfs.length < list.cfs.length){ //если первое меньше
                 for (int i = 0; i < list.cfs.length - cfs.length; i++){
                     tmp.cfs[i] = list.cfs[i] * -1;
                     tmpLen++;
@@ -68,7 +68,7 @@ public class Polynomial {
                 }
 
             }
-            else{ // если первое больше
+            else{ //если первое больше
                 for (int i = 0; i < cfs.length - list.cfs.length; i++){
                     tmp.cfs[i] = cfs[i];
                     tmpLen++;
@@ -82,7 +82,7 @@ public class Polynomial {
         return tmp;
     }
 
-    public Polynomial times(Polynomial list) { // умножение многочленов
+    public Polynomial times(Polynomial list) { //умножение многочленов
         if (cfs.length == 0 && list.cfs.length == 0)
             return new Polynomial();
         else{
@@ -95,7 +95,7 @@ public class Polynomial {
         }
     }
 
-    public int evaluate(int num) { // вычисление значения в точке
+    public int evaluate(int num) { //вычисление значения в точке
         int result = 0;
         for (int i = 0; i < cfs.length; i++){
             result += (int) (cfs[i] * Math.pow(num, cfs.length - i - 1));
@@ -103,7 +103,7 @@ public class Polynomial {
         return result;
     }
 
-    public Polynomial differentiate(int num) { // взятие производной
+    public Polynomial differentiate(int num) { //взятие производной
         for (int i = 0; i < cfs.length; i++) {
             if (i == cfs.length - 1)
                 cfs[i] = 0;
@@ -122,7 +122,7 @@ public class Polynomial {
         }
     }
 
-    public boolean compare(Polynomial list) { // сравнение многочленов
+    public boolean compare(Polynomial list) { //сравнение многочленов
         if (cfs.length != list.cfs.length)
             return false;
         else{
@@ -134,7 +134,7 @@ public class Polynomial {
         }
     }
 
-    public String toString() { // получение строкового представления полинома
+    public String toString() { //получение строкового представления полинома
         StringBuilder word = new StringBuilder();
         for (int i = 0; i < cfs.length; i++){
             if (cfs[i] != 0){
