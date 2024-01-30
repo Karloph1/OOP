fdfafe3342586992cbcc9588f1bbd7805cf1983d
Karloph1 <Karloph1@users.noreply.github.com>
2024-01-30 06:50:09 +0000

    Deploying to gh-pages from @ Karloph1/OOP@aa9884af485dd3a1a700efa5d0d6a4851ad6ed02 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.example","c":"StringFinder","l":"find(String, String)","url":"find(java.lang.String,java.lang.String)"},{"p":"org.example","c":"StringFinder","l":"StringFinder()","url":"%3Cinit%3E()"}]
