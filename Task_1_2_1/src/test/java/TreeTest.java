936ea5c28b4003c5c50e6abc7f7cfeeea4833e66
Karloph <v.labusov@g.nsu.ru>
2024-02-10 13:31:39 +0700

    Task_1_2_1

---
+++ Task_1_2_1/src/test/java/TreeTest.java
@@ -0,0 +1,80 @@
+import org.example.Tree;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TreeTest {
+
+    @Test
+    void testDfsIterator() {
+        Tree<String> tree = new Tree<>("R1");
+        var a = tree.addChild("A");
+        var b = a.addChild("B");
+        Tree<String> subtree = new Tree<>("R2");
+        subtree.addChild("C");
+        subtree.addChild("D");
+        tree.addChild(subtree);
+        b.remove();
+
+        List<String> DfsArray = new ArrayList<>();
+        Iterator iter = tree.iteratorDFS();
+        while (iter.hasNext()) {
+            DfsArray.add(iter.next().toString());
+        }
+        
+        String[] arr = new String[]{"R1", "R2", "D", "C", "A"};
+        if (arr.length != DfsArray.size()) {
+            Assertions.fail();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (DfsArray.get(i) != arr[i]) {
+                Assertions.fail();
+            }
+        }
+    }
+
+    @Test
+    void testBfsIterator() {
+        Tree<String> tree = new Tree<>("R1");
+        var a = tree.addChild("A");
+        var b = a.addChild("B");
+        Tree<String> subtree = new Tree<>("R2");
+        subtree.addChild("C");
+        subtree.addChild("D");
+        tree.addChild(subtree);
+        b.remove();
+
+        List<String> BfsArray = new ArrayList<>();
+        Iterator iter = tree.iteratorBFS();
+        while (iter.hasNext()) {
+            BfsArray.add(iter.next().toString());
+        }
+        String[] arr = new String[]{"R1", "A", "R2", "C", "D"};
+        if (arr.length != BfsArray.size()) {
+            Assertions.fail();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (BfsArray.get(i) != arr[i]) {
+                Assertions.fail();
+            }
+        }
+    }
+
+    @Test
+    void testEquality(){
+        Tree<String> tree1 = new Tree<>("R1");
+        tree1.addChild("A");
+        tree1.addChild("B");
+
+        Tree<String> tree2 = new Tree<>("R1");
+        tree2.addChild("A");
+        tree2.addChild("B");
+        var a = tree1.addChild("C");
+        a.remove();
+        Assertions.assertEquals(tree1, tree2);
+    }
+}
