5a374daff503d4d79aab76243a3e9c5131bc0dc5
Karloph1 <Karloph1@users.noreply.github.com>
2024-03-04 15:43:28 +0000

    Deploying to gh-pages from @ Karloph1/OOP@66838eff27c229c325118967c71e0e68eb79ff8a 🚀

---
+++ Task_2_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Baker"},{"p":"org.example","l":"Bakery"},{"p":"org.example","l":"Courier"},{"p":"org.example","l":"Order"},{"p":"org.example","l":"OrderGenerator"},{"p":"org.example","l":"Parser"},{"p":"org.example","l":"Storage"}]
