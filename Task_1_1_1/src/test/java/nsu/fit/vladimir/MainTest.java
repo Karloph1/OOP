bd34c733f64b93035cf0c5039e70ba077346148e
Karloph <v.labusov@g.nsu.ru>
2024-09-10 13:04:49 +0700

    Add more tests

--- Task_1_1_1/src/test/java/nsu/fit/vladimir/MainTest.java
+++ Task_1_1_1/src/test/java/nsu/fit/vladimir/MainTest.java
@@ -31,4 +31,25 @@ class MainTest {
         Main.heapsort(ans);
         Assertions.assertArrayEquals(new int[]{}, ans);
     }
+
+    @Test
+    void heapsort5() {
+        int[] ans = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Main.heapsort(ans);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, ans);
+    }
+
+    @Test
+    void heapsort6() {
+        int[] ans = {5, 5, 5, 5, 5};
+        Main.heapsort(ans);
+        Assertions.assertArrayEquals(new int[]{5, 5, 5, 5, 5}, ans);
+    }
+
+    @Test
+    void heapsort7() {
+        int[] ans = {10, -23, 9, 0, -23, 0, 0, -3, 100, 23, 10, -23};
+        Main.heapsort(ans);
+        Assertions.assertArrayEquals(new int[]{-23, -23, -23, -3, 0, 0, 0, 9, 10, 10, 23, 100}, ans);
+    }
 }
