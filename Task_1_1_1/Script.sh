308a5505644964cfde441961caa28fbfd345e43a
Karloph <v.labusov@g.nsu.ru>
2024-09-10 13:42:52 +0700

    Improve shell file

--- Task_1_1_1/Script.sh
+++ Task_1_1_1/Script.sh
@@ -1,2 +1,4 @@
-javadoc -d src/docs src/main/java/nsu/fit/vladimir/Main.java
-javac src/main/java/nsu/fit/vladimir/Main.java
+javadoc -d docs src/main/java/nsu/fit/vladimir/Main.java
+javac -d out src/main/java/nsu/fit/vladimir/Main.java
+jar cvf out/app.jar -C out .
+java -cp out nsu.fit.vladimir.Main
