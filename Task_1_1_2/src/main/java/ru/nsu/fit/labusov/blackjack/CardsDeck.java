392d437475d3cff6660728a71dce4450df389030
Karloph <v.labusov@g.nsu.ru>
2024-10-21 15:20:07 +0700

    Remove long lines for checkstyle

--- Task_1_1_2/src/main/java/ru/nsu/fit/labusov/blackjack/CardsDeck.java
+++ Task_1_1_2/src/main/java/ru/nsu/fit/labusov/blackjack/CardsDeck.java
@@ -18,13 +18,16 @@ public class CardsDeck {
 
         for (int i = 2; i < 15; i++) {
             if (i == 14) {
-                cards.addAll(Arrays.stream(CardSuit.values()).map(suit -> new Card(suit, 14, 11)).collect(Collectors.toList()));
+                cards.addAll(Arrays.stream(CardSuit.values()).
+                        map(suit -> new Card(suit, 14, 11)).collect(Collectors.toList()));
             } else if (i > 10) {
                 final int j = i;
-                cards.addAll(Arrays.stream(CardSuit.values()).map(suit -> new Card(suit, j, 10)).collect(Collectors.toList()));
+                cards.addAll(Arrays.stream(CardSuit.values()).
+                        map(suit -> new Card(suit, j, 10)).collect(Collectors.toList()));
             } else {
                 final int k = i;
-                cards.addAll(Arrays.stream(CardSuit.values()).map(suit -> new Card(suit, k, k)).collect(Collectors.toList()));
+                cards.addAll(Arrays.stream(CardSuit.values()).
+                        map(suit -> new Card(suit, k, k)).collect(Collectors.toList()));
             }
         }
     }
