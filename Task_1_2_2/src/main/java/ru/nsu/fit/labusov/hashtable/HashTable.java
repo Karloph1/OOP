08420f549f8d8fa774fd96896649ab46a85050cb
Karloph <v.labusov@g.nsu.ru>
2024-11-09 09:40:02 +0700

    First upload of Task_1_2_2

---
+++ Task_1_2_2/src/main/java/ru/nsu/fit/labusov/hashtable/HashTable.java
@@ -0,0 +1,160 @@
+package ru.nsu.fit.labusov.hashtable;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class HashTable<K, V> implements Iterator<AbstractMap.SimpleEntry<K, V>> {
+    private final ArrayList<K> keys;
+    private final ArrayList<V> values;
+    private int keysIndex = 0;
+
+    public HashTable() {
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
+    }
+
+    public void put(K key, V value) {
+        try {
+            int index = keys.indexOf(key);
+
+            if (index == -1) {
+                keys.add(key);
+                values.add(value);
+            } else {
+                values.set(index, value);
+            }
+        } catch (ConcurrentModificationException e) {
+            System.out.println(e.getMessage());
+            throw new ConcurrentModificationException("Impossible to do this during iteration");
+        }
+
+    }
+
+    public void remove(K key, V value) {
+        try {
+            if (!keys.contains(key) || !value.equals(this.get(key))) {
+                throw new NoSuchElementException("No such pair in hash table");
+            }
+            int index = keys.indexOf(key);
+
+            keys.remove(key);
+            values.remove(index);
+        } catch (ConcurrentModificationException e) {
+            System.out.println(e.getMessage());
+            throw new ConcurrentModificationException("Impossible to do this during iteration");
+        }
+    }
+
+    public void update(K key, V value) {
+        try {
+            int index = keys.indexOf(key);
+
+            values.set(index, value);
+        } catch (ConcurrentModificationException e) {
+            System.out.println(e.getMessage());
+            throw new ConcurrentModificationException("Impossible to do this during iteration");
+        }
+    }
+
+    public V get(K key) {
+        int index = keys.indexOf(key);
+
+        if (index == -1) {
+            return null;
+        }
+
+        return values.get(index);
+    }
+
+    public boolean hasValue(K key) {
+        int index = keys.indexOf(key);
+
+        return index != -1 && values.get(index) != null;
+    }
+
+    public int size() {
+        return keys.size();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        HashTable<Object, Object> table = (HashTable<Object, Object>) obj;
+
+        if (this.size() == 0 && this.size() == table.size()) {
+            return true;
+        }
+
+        if (this.size() != table.size()) {
+            return false;
+        }
+
+        if (this.keys.get(0).getClass() != table.keys.get(0).getClass()) {
+            return false;
+        }
+
+        while (this.hasNext()) {
+            K thisKey = this.next().getKey();
+            V thisValue = this.get(thisKey);
+
+            if (thisValue == null && table.get(thisKey) != null || thisValue != null && table.get(thisKey) == null) {
+                return false;
+            }
+
+            if (!table.keys.contains(thisKey)) {
+                return false;
+            }
+
+            if (thisValue != null && table.get(thisKey) != null) {
+                if (!table.get(thisKey).equals(thisValue)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(keys, values);
+    }
+
+    @Override
+    public String toString() {
+        if (values.isEmpty()) {
+            return "{}";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for (K key : keys) {
+            stringBuilder.append(key).append("=").append(values.get(keys.indexOf(key))).append(", ");
+        }
+
+        return stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}").toString();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return keysIndex < keys.size();
+    }
+
+    @Override
+    public AbstractMap.SimpleEntry<K, V> next() {
+        if (keysIndex < keys.size()) {
+            return new AbstractMap.SimpleEntry<>(keys.get(keysIndex), this.get(keys.get(keysIndex++)));
+        }
+
+        throw new NoSuchElementException("No elements");
+    }
+}
---
+++ Task_1_2_2/src/test/java/ru/nsu/fit/labusov/hashtable/HashTableTest.java
@@ -0,0 +1,90 @@
+package ru.nsu.fit.labusov.hashtable;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HashTableTest {
+
+    @Test
+    void hashTableCreateTest() {
+        HashTable<Object, Object> c = new HashTable<>();
+
+        Assertions.assertEquals(new HashTable<>(), c);
+    }
+
+    @Test
+    void hashTableGetTest() {
+        HashTable<Integer, Integer> c = new HashTable<>();
+        c.put(1, 5);
+
+        Assertions.assertEquals(5, c.get(1));
+    }
+
+    @Test
+    void hashTableUpdateTest() {
+        HashTable<Integer, Integer> c = new HashTable<>();
+        c.put(20, 10);
+        Assertions.assertEquals(10, c.get(20));
+
+        c.update(20, 20);
+        Assertions.assertEquals(20, c.get(20));
+    }
+
+    @Test
+    void hashTableHasValueTest() {
+        HashTable<Integer, Integer> c = new HashTable<>();
+        c.put(10, 5);
+
+        Assertions.assertTrue(c.hasValue(10));
+        Assertions.assertFalse(c.hasValue(1));
+    }
+
+    @Test
+    void hashTableSizeTest() {
+        HashTable<Integer, Integer> c = new HashTable<>();
+        c.put(10, 5);
+        c.put(2, 4);
+
+        Assertions.assertEquals(2, c.size());
+    }
+
+    @Test
+    void hashTableEqualsTest() {
+        HashTable<Integer, Double> c = new HashTable<>();
+        c.put(10, 1.0);
+        c.put(2, null);
+        c.put(-10, null);
+
+        HashTable<Integer, Integer> c1 = new HashTable<>();
+        c1.put(2, null);
+        c1.put(-10, null);
+        c1.put(10, 1);
+
+        Assertions.assertNotEquals(c1, c);
+    }
+
+    @Test
+    void hashTableToStringTest() {
+        HashTable<Integer, Integer> c = new HashTable<>();
+        c.put(10, 5);
+        c.put(2, 4);
+
+        Assertions.assertEquals("{10=5, 2=4}", c.toString());
+
+        HashTable<Integer, Integer> c1 = new HashTable<>();
+        Assertions.assertEquals("{}", c1.toString());
+    }
+
+    @Test
+    void hashTableHasNext() {
+        HashTable<Integer, Integer> c = new HashTable<>();
+        c.put(10, 5);
+        c.put(2, 4);
+
+        Assertions.assertTrue(c.hasNext());
+
+        c.remove(10, 5);
+        c.remove(2, 4);
+        Assertions.assertFalse(c.hasNext());
+    }
+}
