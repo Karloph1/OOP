380e477599f7be16155bbdeba9b12dd2c21c25b3
Karloph <v.labusov@g.nsu.ru>
2025-02-23 14:56:09 +0700

    Forgot to add 3 new classes to previous commit

---
+++ Task_2_1_1/src/main/java/ru/nsu/fit/labusov/primenumbers/Method.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.labusov.primenumbers;
+
+import java.util.ArrayList;
+
+/**
+ * Method interface.
+ */
+public interface Method {
+    boolean hasComplexNum(ArrayList<Integer> row);
+}
